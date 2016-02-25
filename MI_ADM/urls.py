d3a80f761a66f67049a8a8780f79af6b3c1f6543
martin-sicho <sicho.martin@gmail.com>
2016-02-25 21:56:10 +0100

    initial commit for the compound-db app (cherry picked from commit 3093f3a)

--- MI_ADM/urls.py
+++ MI_ADM/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import compound_db.urls
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^compund-db/', include(compound_db.urls))
 ]
