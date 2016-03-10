3831f07960528183c421a2eb6c6d2fd021116e02
martin-sicho <sicho.martin@gmail.com>
2016-03-10 11:57:16 +0100

    removed vagrant related files

--- provisions/bootstrap_python.sh
+++
@@ -1,8 +0,0 @@
-function install_python_packages {
-  CONDA_BIN=$1/bin
-  PATH="$CONDA_BIN:$PATH"
-  echo $PATH
-  conda install -y -f -c https://conda.anaconda.org/rdkit django rdkit bootstrap3 && pip install django-bootstrap3
-}
-
-install_python_packages $1
